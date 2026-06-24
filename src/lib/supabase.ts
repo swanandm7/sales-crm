c843d7a66e7e658ec1fa63349088e1df3adfc449
IshanMaheshwari-777 <ishan77maheshwari@gmail.com>
2026-06-24 12:20:03 +0530

    Fixed:prod error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,9 +4,6 @@ import type { Database } from './database.types';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("URL:", import.meta.env.VITE_SUPABASE_URL)
-console.log("KEY EXISTS:", !!import.meta.env.VITE_SUPABASE_ANON_KEY)
-
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
 }
